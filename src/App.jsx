bc7b87c77253e353918517b2374079f5270f8bcc
Davinder Pal <davinder@solidevelectrosoft.com>
2025-12-12 12:11:27 +0530

    -deploy

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/">
+    <Router basename="/solidevelectrosoft-v4">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
